2d394a005f6dd4e18c39863158d3fb45fce017a4
rafy <rafy.amgadbenjamin@gmail.com>
2025-04-16 15:22:16 +0200

    clean: code cleaning

--- server/app/rest_api/depends.py
+++ server/app/rest_api/depends.py
@@ -1,8 +1,4 @@
-from fastapi import Depends
-
 from app.core.entities import Person
-from app.core.repository import Repositories
-from app.database import DatabaseDriver, get_db_driver
 
 
 def get_user() -> Person:
@@ -10,9 +6,3 @@ def get_user() -> Person:
         id="fd2a5fdf-366f-4a7a-b21c-d31d6eee6c76",
         name="John Smith",
     )
-
-
-def get_repositories(
-    db_driver: DatabaseDriver = Depends(get_db_driver),
-) -> Repositories:
-    return Repositories(db_driver)
